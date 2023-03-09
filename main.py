84e7aa8535f1e8e7d54888d5d4ee733bfb0b0959
TonyDamov <antoan.damov@gmail.com>
2023-03-09 12:10:29 +0200

    Added Color to the tablet and added folder with images

--- main.py
+++ main.py
@@ -1,5 +1,5 @@
 import pygame
-
+import os
 pygame.init()
 
 WIDTH = 1280
@@ -7,16 +7,19 @@ HEIGHT = 720
 SCR = pygame.display.set_mode((WIDTH, HEIGHT))
 COL = (69, 69, 69)
 pygame.display.set_caption('Ohio Security')
-
+TAB_image = pygame.image.load(os.path.join('Images', 'TabletWHandsColor.png'))
+TAB_WIDTH, TAB_HEIGHT = 1200, 605 
+TAB = pygame.transform.scale(TAB_image, (TAB_WIDTH, TAB_HEIGHT))
 FPS = 60
 
 
-def draw_window():
+def draw_window(tab):
     SCR.fill(COL)
+    SCR.blit(TAB, (tab.x, tab.y))
     pygame.display.update()
 
 def main():
-
+    tab = pygame.Rect(WIDTH//2 - TAB_WIDTH//2, HEIGHT - TAB_HEIGHT, TAB_WIDTH, TAB_HEIGHT)
     clock = pygame.time.Clock()
 
     run = True
@@ -27,7 +30,7 @@ def main():
                 run = False
                 pygame.quit()
    
-        draw_window()
+        draw_window(tab)
 main()
 
 if __name__ == "__main__":
